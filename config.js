c257ef38598b00715b04ae44b3172546ec704a30
ChabaJosa <chabagjg@gmail.com>
2020-07-23 11:17:22 -0400

    Working through deployment

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://StageWood:DpnL$ytH9yf^8q(@graphcluster.svz0k.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY:
+    'ALPHA_BRAVO_CHARLIE'
+};
